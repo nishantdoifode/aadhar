e9931eefb36d63c089ba06e6b6342bbf04966be9
nishantdoifode <59484607+nishantdoifode@users.noreply.github.com>
2026-09-23 21:47:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Components
 import NewsMediaSection from './components/Media/NewsMediaSection';
-import Navbar from './components/navbar/Navbar'
-import Optionnavbar from './components/navbar/Optionnavbar'
-import Footer from './components/Footer/Footer'
-import Ecosystem from './components/Ecosystem/Ecosystem'
-import Welcomepage from './Welcomepage'
-import Mainpagedata1 from './components/Homepagecontent/Mainpagedata1'
-import Aboutpage from './components/About/Aboutpage'
+import Ecosystem from './components/Ecosystem/Ecosystem';
+import Welcomepage from './Welcomepage';
+import Mainpagedata1 from './components/Homepagecontent/Mainpagedata1';
+import Aboutpage from './components/About/Aboutpage';
 import Contact from './components/Contact/Contact';
 import ImageSlider from './components/navbar/Imageslider';
 import Optionsnavbar from './components/navbar/Optionsnavbar';
@@ -23,81 +17,55 @@ import Updateaadhaar from './components/Homepagecontent/Updateaadhaar';
 import Geta from './components/Homepagecontent/Geta';
 import Aadhaarservices from './components/Homepagecontent/Aadhaarservices';
 
+// New Layout Component
+import MainLayout from './components/Layout/MainLayout'; 
+
 const App = () => {
   return (
     <Router>
       <Routes>
-
-
+        {/* Standalone page without main layout */}
         <Route path="/" element={<Welcomepage />} />
-        <Route path='/Home' element={
-          <>
 
-            <Navbar />
-            <Optionnavbar />
-            <ImageSlider />
-            <Optionsnavbar />
-            <Mainpagedata1 />
-            <Footer />
+        {/* All other pages wrapped in MainLayout */}
+        <Route path="/" element={<MainLayout />}>
+          
+          {/* Home Page */}
+          <Route index element={
+            <>
+              <ImageSlider />
+              <Optionsnavbar />
+              <Mainpagedata1 />
+            </>
+          } />
 
-          </>
-        } />
-
-        <Route path='/About' element={
-          <>
-            <Navbar />
-            <Optionnavbar />
-           
-            <Optionsnavbar />
-            <Aboutpage />
-            <Footer />
-
-          </>
-        } />
-        <Route path='/Ecosystem' element={
-          <>
-            <Navbar />
-            <Optionnavbar />
-            <Ecosystem />
-            <Footer />
-
-          </>
-        } />
-        <Route path='/Media' element={
-          <>
-            <Navbar />
-            <Optionnavbar />
-            <NewsMediaSection />
-            <Footer />
-
-          </>
-        } />
-        <Route path='/Contact' element={
-          <>
-            <Navbar />
-            <Optionnavbar />
-            <Contact />
-            <Footer />
-
-          </>
-        } />
-
-        <Route path='/aadhaar-status' element={<><Aadhaarstatus /></>} />
-        <Route path='/download-aadhaar' element={<><Navbar /><Optionnavbar /><DownloadAadhaar /><Footer /></>} />
-        <Route path='/aadhaar-status' element={<><Navbar /><Optionnavbar /><Aadhaarstatus /><Footer /></>} />
-        <Route path='/Dashboard' element={<> <Dashboard /> </>} />
-
-        
-<Route path='/Update-A' element={<><Navbar/><Optionnavbar/><Updateaadhaar/><Footer/></>} />
-<Route path='/Update-B' element={<><Navbar/><Optionnavbar/><Geta/><Footer/></>} />
-<Route path='/Update-C' element={<><Navbar/><Optionnavbar/><Aadhaarservices/><Footer/></>} />
+          {/* Other Pages */}
+          <Route path="Home" element={
+            <>
+              <ImageSlider />
+              <Optionsnavbar />
+              <Mainpagedata1 />
+            </>
+          } />
+          
+          <Route path="About" element={<Aboutpage />} />
+          <Route path="Ecosystem" element={<Ecosystem />} />
+          <Route path="Media" element={<NewsMediaSection />} />
+          <Route path="Contact" element={<Contact />} />
+          
+          <Route path="download-aadhaar" element={<DownloadAadhaar />} />
+          <Route path="aadhaar-status" element={<Aadhaarstatus />} />
+          
+          <Route path="Update-A" element={<Updateaadhaar />} />
+          <Route path="Update-B" element={<Geta />} />
+          <Route path="Update-C" element={<Aadhaarservices />} />
+          
+          {/* Note: If Dashboard needs a different layout, keep it outside this block */}
+          <Route path="Dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
-     
     </Router>
+  );
+};
 
-    
-  )
-  
-}
-
-export default App
+export default App;
